65366f4243bc905fd9a3909387539e0f6922e36f
Jeremy <jeremymasters@gmail.com>
2022-04-27 16:57:57 -0600

    Adding the WEATHER_KEY as an environment variable locally and in Cloud Run

--- main.py
+++ main.py
@@ -2,39 +2,10 @@ import os
 from datetime import datetime
 import requests
 from flask import Flask, render_template
-import google_crc32c
 
 app = Flask(__name__)
 
-
-# [START secretmanager_access_secret_version]
-def get_weather_key():
-    """
-    Access the payload for the given secret version if one exists. The version
-    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
-    """
-    # Import the Secret Manager client library.
-    from google.cloud import secretmanager
-
-    # Create the Secret Manager client.
-    client = secretmanager.SecretManagerServiceClient()
-    project_id = "gooflask"
-    secret_id = "WEATHER_KEY"
-    # Build the resource name of the secret version.
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/1"
-
-    # Access the secret version.
-    response = client.access_secret_version(request={"name": name})
-
-    # Verify payload checksum.
-    crc32c = google_crc32c.Checksum()
-    crc32c.update(response.payload.data)
-    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-        print("Data corruption detected.")
-        return response
-
-    return response.payload.data.decode("utf-8")
-
+WEATHER_KEY = ""
 
 def degrees_to_compass_direction(deg):
     val = int((deg / 22.5) + .5)
@@ -51,7 +22,6 @@ def hello_world():
 @app.route('/weather')
 def get_weather():
 
-    WEATHER_KEY = get_weather_key()
     zip_code = 59718
     country_code = "US"
     my_url = f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&units=imperial&appid={WEATHER_KEY}"
@@ -69,8 +39,6 @@ def get_weather():
 
 @app.route('/5day')
 def get_5day():
-
-    WEATHER_KEY = get_weather_key()
 
     lat = "45.676998"
     lon = "-111.042931"
@@ -96,4 +64,5 @@ def get_5day():
 
 
 if __name__ == "__main__":
+    WEATHER_KEY = os.getenv("WEATHER_KEY")
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
